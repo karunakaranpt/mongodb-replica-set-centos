a3acc5b4e31fe1a7c38b87a6280668b79459218c
Karunakaran <mkarunakaran@paxata.com>
2018-03-19 12:13:38 +0530

    Initial commit

---
+++ scripts/secondary.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+replSetName=$1
+zabbixServer=$2
+
+
+
+disk_format() {
+	cd /tmp
+	apt-get install wget -y
+	for ((j=1;j<=3;j++))
+	do
+		wget https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/shared_scripts/ubuntu/vm-disk-utils-0.1.sh 
+		if [[ -f /tmp/vm-disk-utils-0.1.sh ]]; then
+			bash /tmp/vm-disk-utils-0.1.sh -b /var/lib/mongo -s
+			if [[ $? -eq 0 ]]; then
+				sed -i 's/disk1//' /etc/fstab
+				umount /var/lib/mongo/disk1
+				mount /dev/md0 /var/lib/mongo
+			fi
+			break
+		else
+			echo "download vm-disk-utils-0.1.sh failed. try again."
+			continue
+		fi
+	done
+		
+}
+
+
+install_mongo3() {
+
+	# Import the public key
+	echo "Import the public key used by the package management system"
+	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+
+	# Create a list file
+	echo "Create a list file for MongoDB"
+	echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
+	# Install update
+	echo "Install apt-get update"
+	apt-get -y update
+
+	# Install MongoDB
+	echo "Install mongodb"
+	apt-get -y install mongodb-org
+
+	#kernel settings
+	if [[ -f /sys/kernel/mm/transparent_hugepage/enabled ]];then
+		echo never > /sys/kernel/mm/transparent_hugepage/enabled
+	fi
+	if [[ -f /sys/kernel/mm/transparent_hugepage/defrag ]];then
+		echo never > /sys/kernel/mm/transparent_hugepage/defrag
+	fi
+
+	#configure
+	sed -i 's/\(bindIp\)/#\1/' /etc/mongod.conf
+}
+
+
+install_zabbix() {
+	#install zabbix agent
+	cd /tmp
+	apt-get install -y gcc wget > /dev/null
+	wget http://jaist.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/2.2.5/zabbix-2.2.5.tar.gz > /dev/null
+	tar zxvf zabbix-2.2.5.tar.gz
+	cd zabbix-2.2.5
+	groupadd zabbix
+	useradd zabbix -g zabbix -s /sbin/nologin
+	mkdir -p /usr/local/zabbix
+	./configure --prefix=/usr/local/zabbix --enable-agent
+	make install > /dev/null
+	cp misc/init.d/fedora/core/zabbix_agentd /etc/init.d/
+	sed -i 's/BASEDIR=\/usr\/local/BASEDIR=\/usr\/local\/zabbix/g' /etc/init.d/zabbix_agentd
+	sed -i '$azabbix-agent    10050/tcp\nzabbix-agent    10050/udp' /etc/services
+	sed -i '/^LogFile/s/tmp/var\/log/' /usr/local/zabbix/etc/zabbix_agentd.conf
+	hostName=`hostname`
+	sed -i "s/^Hostname=Zabbix server/Hostname=$hostName/" /usr/local/zabbix/etc/zabbix_agentd.conf
+	if [[ $zabbixServer =~ ([0-9]{1,3}.){3}[0-9]{1,3} ]];then
+		sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
+		sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agentd.conf
+		sed -i "s/^Server=127.0.0.1/Server=$zabbixServer/" /usr/local/zabbix/etc/zabbix_agent.conf
+	fi
+	touch /var/log/zabbix_agentd.log
+	chown zabbix:zabbix /var/log/zabbix_agentd.log
+
+	#start zabbix agent
+	chkconfig --add zabbix_agentd
+	chkconfig zabbix_agentd on
+	/etc/init.d/zabbix_agentd start
+
+}
+
+install_mongo3
+disk_format
+install_zabbix
+
+#start replica set
+mongod --dbpath /var/lib/mongo/ --config /etc/mongod.conf --replSet $replSetName --logpath /var/log/mongodb/mongod.log --fork
+
+
+#check if mongod started or not
+sleep 15
+n=`ps -ef |grep "mongod --dbpath /var/lib/mongo/" |grep -v grep|wc -l`
+if [[ $n -eq 1 ]];then
+    echo "replica set started successfully"
+else
+    echo "replica set started failed!"
+fi
+
+
+#set mongod auto start
+cat > /etc/init.d/mongod1 <<EOF
+#!/bin/bash
+#chkconfig: 35 84 15
+#description: mongod auto start
+. /etc/init.d/functions
+
+Name=mongod1
+start() {
+if [[ ! -d /var/run/mongodb ]];then
+mkdir /var/run/mongodb
+chown -R mongod:mongod /var/run/mongodb
+fi
+mongod --dbpath /var/lib/mongo/ --replSet $replSetName --logpath /var/log/mongodb/mongod.log --fork --config /etc/mongod.conf
+}
+stop() {
+pkill mongod
+}
+restart() {
+stop
+sleep 15
+start
+}
+
+case "\$1" in 
+    start)
+	start;;
+	stop)
+	stop;;
+	restart)
+	restart;;
+	status)
+	status \$Name;;
+	*)
+	echo "Usage: service mongod1 start|stop|restart|status"
+esac
+EOF
+chmod +x /etc/init.d/mongod1
+chkconfig mongod1 on
